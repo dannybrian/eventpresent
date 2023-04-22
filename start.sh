bc6dcab52561935d68be39c8516cd9f1eba28682
Danny Brian <danny@brians.org>
2023-04-22 14:48:37 -0600

    Debugging why flows behave differently based on directory

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 /usr/local/sbin/mosquitto -c ./servers/mosquitto.conf &> ./servers/logs/mosquitto.log &
-node-red -s ./servers/node-red-settings.cjs &> ./servers/logs/node-red.log &
+node-red -s ./servers/node-red-settings.cjs ./servers/node-red-flows.json &> ./servers/logs/node-red.log &
 node servers/admin-node.js &> ./servers/logs/admin-node.log &
 nginx -c servers/nginx.conf -p /Users/dbrian/Sources/eventpresent/
 
