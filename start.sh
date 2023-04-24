cb9d9574df9db0b69316cb7f0503c64a11835d8f
dabrian <dabrian@fvfhx2kjq05n.ent.gartner.com>
2023-04-24 10:09:25 -0600

    using parcel; fixed portability of passwords

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-/usr/local/sbin/mosquitto -c ./servers/mosquitto.conf &> ./servers/logs/mosquitto.log &
+/opt/homebrew/sbin/mosquitto -c ./servers/mosquitto.conf &> ./servers/logs/mosquitto.log &
 node-red -s ./servers/node-red-settings.cjs ./servers/node-red-flows.json &> ./servers/logs/node-red.log &
 node servers/admin-node.js &> ./servers/logs/admin-node.log &
-nginx -c servers/nginx.conf -p /Users/dbrian/Sources/eventpresent/
+nginx -c servers/nginx.conf -p /Users/dabrian/Sources/eventpresent/
 
 
